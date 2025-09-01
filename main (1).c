3c7a92259844914389a2078da6ece76d6e7efd00
Ariya06 <fayzaaliyah.devina@gmail.com>
2025-09-01 16:52:16 +0700

    first commit

---
+++ main (1).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int a, b;
+    float hasil_bagi;
+    a = 25;
+    b = 7;
+    hasil_bagi = (float)a/b;
+    
+    printf("Pertambahan bilangan a dan b: %d\n", a+b);
+    printf("Perkurang bilagan a dan b: %d\n", a-b);
+    printf("perkalian bilangan a dan b: %d\n", a*b);
+    printf("Pembagian bilangan a dan b: %\n", hasil_bagi);
+    return 0;
+}
